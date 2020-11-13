325e9bbe80337f37895fac82339fe7a10dd5c1d6
chenluwei <chenluwei@showplus.cn>
2020-11-13 16:36:08 +0800

    制定按键加载配置项

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,51 @@
+<style>
+  .rect,
+  .circle {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid #eee;
+  }
+  .rect {
+    border-radius: 6rpx;
+  }
+  .circle {
+    border-radius: 50%;
+  }
+  uni-page-body {
+    position: relative;
+    height: 100%;
+  }
+  .content {
+    width: 100%;
+    position: absolute;
+  }
+  .key {
+    position: absolute;
+  }
+</style>
+
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="content" :style="{ height: containerH, top: containerTop}">
+    <view
+        v-for="key in keyMap"
+        :key="key.id"
+        class="key"
+        :class="key.face.shape === 'rect' ? 'rect' : 'circle'"
+        :style="{
+          top: key.seat.y * sizeRate  + 'px',
+          left: key.seat.x * sizeRate + 'px',
+          width: key.face.width * sizeRate + 'px',
+          height: key.face.height * sizeRate + 'px',
+          borderColor: key.face.borderColor,
+          color: key.face.color,
+          backgroundColor: key.face.backgroundColor
+        }"
+    >
+      {{ key.content }}
+    </view>
 	</view>
 </template>
 
@@ -11,39 +53,66 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+        containerH: '550px',
+        containerTop: 0,
+        sizeRate: 3.75,
+        keyMap: [
+          {
+            id: '11',
+            face: {
+              width: 10,
+              height: 10,
+              shape: 'circle',
+              borderColor: '#666',
+              color: '#333',
+              backgroundColor: '#eee'
+            },
+            seat: {
+              x: 10,
+              y: 20
+            },
+            content: 'A'
+          },
+          {
+            id: '2',
+            face: {
+              width: 10,
+              height: 10,
+              shape: 'rect',
+              borderColor: '#666',
+              color: '#333',
+              backgroundColor: '#eee'
+            },
+            seat: {
+              x: 30,
+              y: 20
+            },
+            content: 'B'
+          }
+        ]
 			}
 		},
 		onLoad() {
-
+		  const _this = this
+      uni.getSystemInfo({
+        success: function (info) {
+          const width = info.windowWidth
+          const totalHeight = info.windowHeight
+          const pixelRatio = info.pixelRatio
+          const height = Math.ceil(width*1.6)
+          _this.containerH = Math.ceil(width*1.6) + 'px'
+          _this.containerTop = (totalHeight - height)/2 + 'px'
+          _this.sizeRate = width/100
+        },
+        fail: function () {
+          uni.showToast({
+            message: '初始化失败！'
+          })
+        }
+      })
 		},
 		methods: {
 
 		}
 	}
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
